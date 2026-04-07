0b48e25f50f3a64afe6cef56fde8e6e137e9f2b9
Muhammed-Ramadan512 <mohamedramadan949@gmail.com>
2026-04-07 20:17:21 +0200

    Fix compiler error due to new InMeetingServiceListener method in 5.17.11

--- android/src/main/java/com/webcare/zoom/sdksample/inmeetingfunction/customizedmeetingui/SimpleInMeetingListener.java
+++ android/src/main/java/com/webcare/zoom/sdksample/inmeetingfunction/customizedmeetingui/SimpleInMeetingListener.java
@@ -341,4 +341,8 @@ public class SimpleInMeetingListener implements InMeetingServiceListener {
     public void onVideoAlphaChannelStatusChanged(boolean isAlphaModeOn) {
 
     }
+    @Override
+    public void onAllowParticipantsRequestCloudRecording(boolean b) {
+
+    }
 }
